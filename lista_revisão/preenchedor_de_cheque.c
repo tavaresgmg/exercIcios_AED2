1212688552d752596e7a3fc6b8e07fff48564f77
Guilherme Tavares <95603984+tavaresgmg@users.noreply.github.com>
2023-09-26 12:47:40 -0300

    Update preenchedor_de_cheque.c

--- lista_revisão/preenchedor_de_cheque.c
+++ lista_revisão/preenchedor_de_cheque.c
@@ -15,8 +15,6 @@ int main(){
         inteiro = (int) num;
         decimal = (int) (num * 100) % 100;
 
-        
-
         if(inteiro > 1000000){
             milhoes = inteiro / 1000000;
             resto = inteiro % 1000000;
@@ -27,106 +25,141 @@ int main(){
         else if (inteiro > 1000){
 
         }
-
-
     }
 
     return 0;
 }
 
 void unidade (int inteiro){
-    if (inteiro > 1000){
-        if ((inteiro / 1000) == 9000)
-    }
-    else if (inteiro > 100){
+    int dezena, unidade;
 
-    }
-    else if (inteiro > 10){
-
-    }
-
-    switch (){
-        case 1:
-            printf("UM");
-            break;
-        case 2:
-            printf("DOIS");
-            break;
-        case 3:
-            printf("TRES");
-            break;
-        case 4:
-            printf("QUATRO");
-            break;
-        case 5:
-            break;
-        case 6:
-            break;
-        case 7:
-            break;
-        case 8:
-            break;
-        case 9:
-            break;
-        case 10:
-            break;
-        case 11:
-            break;
-        case 12:
-            break;
-        case 13:
-            break;
-        case 14:
-            break;
-        case 15:
-            break;
-        case 16:
-            break;
-        case 17:
-            break;
-        case 18:
-            break;
-        case 19:
-            break;
-        default:
-            break;
+        if(inteiro > 100){
+            switch (inteiro){
+                case 1:
+                    printf("CENTO");
+                    break;
+                case 2:
+                    printf("DUZENTOS");
+                    break;
+                case 3:
+                    printf("TREZENTOS");
+                    break;
+                case 4:
+                    printf("QUATROCENTOS");
+                    break;
+                case 5:
+                    printf("QUINHETOS");
+                    break;
+                case 6: 
+                    printf("SEICENTOS");
+                    break;
+                case 7:
+                    printf("SETECENTOS");
+                    break;
+                case 8:
+                    printf("OITOCENTOS");
+                    break;
+                case 9:
+                    printf("NOVECENTOS");
+                    break;
+            }
         }
-        
-    
-    
 
-    
-        printf("CINCO");
-        printf("SEIS");
-        printf("SETE");
-        printf("OITO");
-        printf("NOVE");
-        printf("DEZ");
-        printf("ONZE");
-        printf("DOZE");
-        printf("TREZE");
-        printf("CATORZE");
-        printf("QUIZE");
-        printf("DEZESSEIS");
-        printf("DEZESSETE");
-        printf("DEZOIT");
-        printf("DEZENOVE");
-        printf("VINTE");
-        printf("TRINTA");
-        printf("QUARENTA");
-        printf("CINQUENTA");
-        printf("SESSENTA");
-        printf("SETENTA");
-        printf("OITENTA");
-        printf("NOVENTA");
-        printf("CEM");
-        printf("DUZENTOS");
-        printf("TREZENTOS");
-        printf("QUATROCENTOS");
-        printf("QUINHETOS");
-        printf("SEICENTOS");
-        printf("SETECENTOS");
-        printf("OITOCENTOS");
-        printf("NOVECENTOS");
-        printf("MIL");
-}
+        if(inteiro > 20 && inteiro < 100){
+            dezena = inteiro / 10;
+            unidade = inteiro % 10;
+
+            switch (inteiro){
+                case 2:
+                    printf("VINTE");
+                    break;
+                case 3:
+                    printf("TRINTA");
+                    break;
+                case 4:
+                    printf("QUARENTA");
+                    break;
+                case 5:
+                    printf("CINQUENTA");
+                    break;
+                case 6:
+                    printf("SESSENTA");
+                    break;
+                case 7:
+                    printf("SETENTA");
+                    break;
+                case 8:
+                    printf("OITENTA");
+                    break;
+                case 9:
+                    printf("NOVENTA");
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+
+    if (inteiro < 20){
+        switch (inteiro){
+            case 1:
+                printf("UM");
+                break;
+            case 2:
+                printf("DOIS");
+                break;
+            case 3:
+                printf("TRES");
+                break;
+            case 4:
+                printf("QUATRO");
+                break;
+            case 5:
+                printf("CINCO");
+                break;
+            case 6:
+                printf("SEIS");
+                break;
+            case 7:
+                printf("SETE");
+                break;
+            case 8:
+                printf("OITO");
+                break;
+            case 9:
+                printf("NOVE");
+                break;
+            case 10:
+                printf("DEZ");
+                break;
+            case 11:
+                printf("ONZE");
+                break;
+            case 12:
+                printf("DOZE");
+                break;
+            case 13:
+                printf("TREZE");
+                break;
+            case 14:
+                printf("CATORZE");
+                break;
+            case 15:
+                printf("QUINZE");
+                break;
+            case 16:
+                 printf("DEZESSEIS");
+                break;
+            case 17:
+                printf("DEZESSETE");
+                break;
+            case 18:
+                printf("DEZOITO");
+                break;
+            case 19:
+                printf("DEZENOVE");
+                break;
+            default:
+                break;
+        }
+    }
