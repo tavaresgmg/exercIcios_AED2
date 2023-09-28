2f94aa84e0a83c8c7c20d23f43cf8f71e176bc9c
Guilherme Tavares <95603984+tavaresgmg@users.noreply.github.com>
2023-09-28 18:27:25 -0300

    Finalização do exercício

--- lista_revisão/preenchedor de cheque/preenchedor_de_cheque.c
+++ lista_revisão/preenchedor de cheque/preenchedor_de_cheque.c
@@ -1,59 +1,97 @@
+
 #include <stdio.h>
 
 void unidade (int inteiro);
 
 int main(){
     int casos; //numero de casos de teste
-    int i, temp, inteiro, decimal, milhoes, resto=0, centenas, milhares;
+    int i, inteiro, decimal, milhoes, resto, milhares;
     float num;
-
+    
     scanf("%d", &casos);
-
+    
     for(i=0;i<casos;i++){
         scanf("%f", &num);
-
+        
+        
         inteiro = (int) num;
         decimal = (int) (num * 100) % 100;
-
-        if(inteiro > 1000000){
-            milhoes = inteiro / 1000000;
-            resto = inteiro % 1000000;
-
-            unidade(milhoes);
-
-            printf("MILHOES ");
-
+        
+        if(inteiro == 1 || inteiro == 100 || inteiro == 1000 || inteiro == 1000000 ){
+            switch (inteiro) {
+                case 1:
+                    printf("UM REAL");
+                    break;
+                case 100:
+                    printf("CEM REAIS");
+                    break;
+                case 1000:
+                    printf("MIL REAIS");
+                    break;
+                case 1000000:
+                    printf("UM MILHÃO ");
+                default:
+                    break;
+            }
+            printf(" ");
+            printf("\n");
+            if (decimal > 0){
+                printf("E ");
+                
+                unidade(decimal);
+                
+                printf("CENTAVOS ");
+                printf("\n");
+            }
         } else {
-            resto = inteiro;
+            
+            
+            if(inteiro > 1000000){
+                milhoes = inteiro / 1000000;
+                resto = inteiro % 1000000;
+                
+                unidade(milhoes);
+                
+                printf("MILHOES ");
+                
+            } else {
+                resto = inteiro;
+            }
+            if(resto > 1000){
+                milhares = resto / 1000;
+                resto = inteiro % 1000;
+                
+                unidade(milhares);
+                
+                printf("MIL ");
+            }
+            
+            unidade(resto);
+            
+            if (resto != 1 ) {
+                printf("REAIS ");
+            }
+            if(decimal == 0){
+                printf("\n");
+            }
+            
+            if (decimal > 0){
+                printf("E ");
+                
+                unidade(decimal);
+                
+                printf("CENTAVOS ");
+                printf("\n");
+            }
         }
-        if(resto > 1000){
-            milhares = resto / 1000;
-            resto = inteiro % 1000;
-
-            unidade(milhares);
-
-            printf("MIL ");
-        }
-
-        unidade(resto);
-
-        printf("REAIS ");
-
-        if (decimal > 0){
-            printf("E ");
-
-            unidade(decimal);
-
-            printf("CENTAVOS ");
-        }
+        
+        
     }
-
     return 0;
 }
-
-void unidade (int inteiro){
-    int dezena, unidade, centenas;
-
+    void unidade (int inteiro){
+        int dezena, centenas;
+        
         if(inteiro > 100){
             centenas = inteiro/100;
             inteiro = inteiro % 100;
@@ -73,7 +111,7 @@ void unidade (int inteiro){
                 case 5:
                     printf("QUINHETOS");
                     break;
-                case 6: 
+                case 6:
                     printf("SEICENTOS");
                     break;
                 case 7:
@@ -87,12 +125,15 @@ void unidade (int inteiro){
                     break;
             }
             printf(" ");
+            if (inteiro > 0){
+                printf("E ");
+            }
         }
-
+        
         if(inteiro >= 20 && inteiro < 100){
             dezena = inteiro / 10;
             inteiro = inteiro % 10;
-
+            
             switch (dezena){
                 case 2:
                     printf("VINTE");
@@ -122,70 +163,74 @@ void unidade (int inteiro){
                     break;
             }
             printf(" ");
+            if (inteiro > 0){
+                printf("E ");
+            }
         }
-    
-    if(inteiro < 20){
-        switch (inteiro){
-            case 1:
-                printf("UM");
-                break;
-            case 2:
-                printf("DOIS");
-                break;
-            case 3:
-                printf("TRES");
-                break;
-            case 4:
-                printf("QUATRO");
-                break;
-            case 5:
-                printf("CINCO");
-                break;
-            case 6:
-                printf("SEIS");
-                break;
-            case 7:
-                printf("SETE");
-                break;
-            case 8:
-                printf("OITO");
-                break;
-            case 9:
-                printf("NOVE");
-                break;
-            case 10:
-                printf("DEZ");
-                break;
-            case 11:
-                printf("ONZE");
-                break;
-            case 12:
-                printf("DOZE");
-                break;
-            case 13:
-                printf("TREZE");
-                break;
-            case 14:
-                printf("CATORZE");
-                break;
-            case 15:
-                printf("QUINZE");
-                break;
-            case 16:
-                 printf("DEZESSEIS");
-                break;
-            case 17:
-                printf("DEZESSETE");
-                break;
-            case 18:
-                printf("DEZOITO");
-                break;
-            case 19:
-                printf("DEZENOVE");
-                break;
-            default:
-                break;
+        
+        if(inteiro < 20){
+            switch (inteiro){
+                case 1:
+                    printf("UM");
+                    break;
+                case 2:
+                    printf("DOIS");
+                    break;
+                case 3:
+                    printf("TRES");
+                    break;
+                case 4:
+                    printf("QUATRO");
+                    break;
+                case 5:
+                    printf("CINCO");
+                    break;
+                case 6:
+                    printf("SEIS");
+                    break;
+                case 7:
+                    printf("SETE");
+                    break;
+                case 8:
+                    printf("OITO");
+                    break;
+                case 9:
+                    printf("NOVE");
+                    break;
+                case 10:
+                    printf("DEZ");
+                    break;
+                case 11:
+                    printf("ONZE");
+                    break;
+                case 12:
+                    printf("DOZE");
+                    break;
+                case 13:
+                    printf("TREZE");
+                    break;
+                case 14:
+                    printf("CATORZE");
+                    break;
+                case 15:
+                    printf("QUINZE");
+                    break;
+                case 16:
+                    printf("DEZESSEIS");
+                    break;
+                case 17:
+                    printf("DEZESSETE");
+                    break;
+                case 18:
+                    printf("DEZOITO");
+                    break;
+                case 19:
+                    printf("DEZENOVE");
+                    break;
+                default:
+                    break;
+            }
+            printf(" ");
         }
-        printf(" ");
     }
-}
+
